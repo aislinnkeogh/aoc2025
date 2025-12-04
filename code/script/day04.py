5515f4b74ddbef32fcd81d1833d6a02393c587ec
Aislinn Keogh <aislinn.k.keogh@gmail.com>
2025-12-04 13:17:36 +0000

    Refactor Day 4 to remove redundancy

--- code/script/day04.py
+++ code/script/day04.py
@@ -9,37 +9,44 @@ diagram = Grid('input/day04.txt')
 def count_rolls(adjacent_positions):
     return adjacent_positions.count('@')
 
-part1 = 0
-rolls_to_remove = []
-for x in range(diagram.shape[1]): # columns
-    for y in range(diagram.shape[0]): # rows
-        if diagram.get(x, y)=='@':
-            adjacent_positions = diagram.get_adjacent_positions(x, y)
-            if count_rolls(adjacent_positions) < 4:
-                part1 += 1
-                rolls_to_remove.append((x, y))
-
-# Remove all rolls identified in Part 1
-for x, y in rolls_to_remove:
-    diagram.update(x, y)
-
-# Start Part 2 counter at the number of rolls removed in Part 1
-part2 = part1
-
-# Then iterate through the grid again, this time removing rolls as we go
-no_more_to_remove = False
-while not no_more_to_remove:
-    removed_this_pass = 0
-    for x in range(diagram.shape[1]):
-        for y in range(diagram.shape[0]):
-            if diagram.get(x, y)=='@':
-                adjacent_positions = diagram.get_adjacent_positions(x, y)
+def find_rolls(grid, remove_immediately):
+    rolls_to_remove = []
+    n_removed = 0
+    for x in range(grid.shape[1]): # columns
+        for y in range(grid.shape[0]): # rows
+            if grid.get(x, y)=='@':
+                adjacent_positions = grid.get_adjacent_positions(x, y)
                 if count_rolls(adjacent_positions) < 4:
-                    diagram.update(x, y)
-                    part2 += 1
-                    removed_this_pass += 1
-    if removed_this_pass==0:
-        no_more_to_remove = True
+                    n_removed += 1
+                    if remove_immediately:
+                        grid.update(x, y)
+                    else:
+                        rolls_to_remove.append((x, y))
+    return rolls_to_remove, n_removed
+
+def solve(diagram):
+    rolls_to_remove, n_removed = find_rolls(diagram, remove_immediately=False)
+    part1 = n_removed
+
+    # Remove all rolls identified in Part 1
+    for x, y in rolls_to_remove:
+        diagram.update(x, y)
+        
+    # Start Part 2 counter at the number of rolls removed in Part 1
+    part2 = part1
+    
+    # Then iterate through the grid again, this time removing rolls as we go
+    no_more_to_remove = False
+    while not no_more_to_remove:
+        rolls_to_remove, removed_this_pass = find_rolls(diagram, remove_immediately=True)
+        if removed_this_pass==0:
+            no_more_to_remove = True
+        else:
+            part2 += removed_this_pass
+    
+    return part1, part2
+
+part1, part2 = solve(diagram)
 
 print(part1)
 print(part2)
