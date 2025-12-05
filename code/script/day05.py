02ae0eabb415e5a350256d470fe52cc580ca045a
Aislinn Keogh <aislinn.k.keogh@gmail.com>
2025-12-05 11:59:57 +0000

    Tidy up file parsing for Day 5

--- code/script/day05.py
+++ code/script/day05.py
@@ -4,15 +4,17 @@ import bisect
 
 with open('input/day05.txt') as f:
     lines = [line.strip() for line in f]
-break_point = lines.index('')
-ranges = [[int(n) for n in r.split("-")] for r in lines[:break_point]]
-ids = [int(n) for n in lines[break_point+1:]]
 
-def in_range(n, ranges, startpoints):
-    i = bisect.bisect_right(startpoints, n) - 1
+def parse(lines):
+    break_point = lines.index('')
+    ranges = [[int(n) for n in r.split("-")] for r in lines[:break_point]]
+    ids = [int(n) for n in lines[break_point+1:]]
+    return ranges, ids
+
+def in_ranges(n, ranges, startpoints):
+    i = bisect.bisect_right(startpoints, n) - 1 # bisect_right --> where would we have to insert this number to preserve the order? need to check the range at the index before this
     if i >= 0:
-        start, stop = ranges[i]
-        return start <= n <= stop
+        return ranges[i][0] <= n <= ranges[i][1]
     return False
 
 def solve(ranges, ids):
@@ -24,11 +26,11 @@ def solve(ranges, ids):
         else: # overlap: set the end point of the previous range to whichever is higher out of it and the end point of the current range
             merged_ranges[-1][1] = max(merged_ranges[-1][1], stop)
     startpoints = [r[0] for r in merged_ranges]
-    # part1 = sum(ingredient >= start and ingredient <= stop for ingredient in ids for start, stop in merged_ranges)
-    part1 = sum(in_range(ingredient, merged_ranges, startpoints) for ingredient in ids)
+    part1 = sum(in_ranges(ingredient, merged_ranges, startpoints) for ingredient in ids)
     part2 = sum(stop - start + 1 for start, stop in merged_ranges)
     return part1, part2
 
+ranges, ids = parse(lines)
 part1, part2 = solve(ranges, ids)
 
 print(part1)
